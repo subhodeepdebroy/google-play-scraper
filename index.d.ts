32f572578f397319024c53103187e3cd4caf01e1
Subhodeep Debory <subhodeep12345@gmail.com>
2023-08-31 16:26:03 +0530

    Added support for custom gplay url

--- index.d.ts
+++ index.d.ts
@@ -192,7 +192,7 @@ export interface IFnAppOptions extends IOptions {
 }
 
 export interface IFnApp {
-  (options: IFnAppOptions): Promise<IAppItemFullDetail>
+  (options: IFnAppOptions, url: string): Promise<IAppItemFullDetail>
 }
 
 // -- list()
